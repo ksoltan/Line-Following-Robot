77e1122af09b9b53ab1db329030fb2e803019c46
Regina Walker <reginamwalker19@gmail.com>
2016-10-11 10:49:53 -0400

    Calibrated the sensors. Starboard detection works fine, port does not. Needs fix

--- Line-Following-Robot.ino
+++ Line-Following-Robot.ino
@@ -1,47 +1,63 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_PWMServoDriver.h"
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *portMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *starboardMotor = AFMS.getMotor(4);
+Adafruit_DCMotor *portMotor = AFMS.getMotor(4);
+Adafruit_DCMotor *starboardMotor = AFMS.getMotor(1);
 
 
 boolean isOffCoursePort = false;
 boolean isOffCourseStarboard = false;
+int port = A0;
+int starboard = A1;
 
 void setup() {
   AFMS.begin();
-  portMotor->setSpeed(10);
-  starboardMotor->setSpeed(10);
+  portMotor->setSpeed(20);
+  starboardMotor->setSpeed(20);
   Serial.begin(9600);
-  int port = A0;
-  int starboard = A1;
 }
 
 void loop() {
   int portValue = analogRead(port);
   int starboardValue = analogRead(starboard);
-  if (isSensorOverTape(portValue)) {
-    isOffCoursePort = true;
-    portMotor->run(Release);
+  if (isPortOverTape(portValue)) {
+    portMotor->run(RELEASE);
+    starboardMotor->run(FORWARD);
     //do something
   }
-  if (isSensorOverTape(starboardValue)) {
-    isOffCourseStarboard = true;
-    starboardMotor->run(Release);
+  if (isStarboardOverTape(starboardValue)) {
+    starboardMotor->run(RELEASE);
+    portMotor->run(FORWARD);
     //do the other thing
   }
   
   // If neither of the sensors are over tape,
   // continue straight and true
   if (!isOffCoursePort && !isOffCourseStarboard){
-    portMotor->run(Forward);
-    starboardMotor->run(Forward);
+    portMotor->run(FORWARD);
+    starboardMotor->run(FORWARD);
     // make sure both motors are going at same power
   }
 }
 
-boolean isSensorOverTape(int sensorValue){
-  return false;
-  // TODO
+boolean isPortOverTape(int sensorValue){
+  boolean sensorOverTape = sensorValue > 500;
+  if(sensorOverTape){
+    isOffCoursePort = true;
+  }else{
+    isOffCoursePort = false;
+  }
+  return sensorOverTape;
 }
+
+boolean isStarboardOverTape(int sensorValue){
+  boolean sensorOverTape = sensorValue > 500;
+  if(sensorOverTape){
+    isOffCourseStarboard = true;
+  }else{
+    isOffCourseStarboard = false;
+  }
+  return sensorOverTape;
+}
+
