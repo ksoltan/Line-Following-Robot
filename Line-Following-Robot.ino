693daf40ceeb9b053b0a87244e69d6d6d85b2d2f
Regina Walker <reginamwalker19@gmail.com>
2016-10-13 17:40:16 -0400

    Changed motor speed and changed turning to be one wheel forwards and one backwards

--- Line-Following-Robot.ino
+++ Line-Following-Robot.ino
@@ -13,33 +13,38 @@ int starboard = A1;
 
 void setup() {
   AFMS.begin();
-  portMotor->setSpeed(20);
-  starboardMotor->setSpeed(20);
+  portMotor->setSpeed(17);
+  starboardMotor->setSpeed(17);
   Serial.begin(9600);
 }
 
 void loop() {
   // Assume that only one out of the two sensors in over the tape at any point
   if (isPortOverTape()) {
-    portMotor->run(RELEASE);
+    portMotor->run(BACKWARD);
     starboardMotor->run(FORWARD);
   }
   else if (isStarboardOverTape()) {
-    starboardMotor->run(RELEASE);
+    starboardMotor->run(BACKWARD);
     portMotor->run(FORWARD);
-  } else if (!isOffCoursePort && !isOffCourseStarboard){
+  }
+  else if (!isOffCoursePort && !isOffCourseStarboard){
     portMotor->run(FORWARD);
     starboardMotor->run(FORWARD);
     // make sure both motors are going at same power
   }
+  else if (isOffCoursePort && isOffCourseStarboard){
+    portMotor->run(FORWARD);
+    starboardMotor->run(FORWARD);
+    // if both sensors are on the tape, run it forward
+  }
 }
-
 /*Checks if the port sensor is over the tape.
 If not, sets the offCourse flag to false. If off tape, sets the flag to true.
 Returns isOffCoursePort*/
 boolean isPortOverTape(){
   int portValue = analogRead(port);
-  isOffCoursePort = portValue > 500;
+  isOffCoursePort = portValue > 300;
   return isOffCoursePort;
 }
 
